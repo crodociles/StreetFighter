1f93df3a748594f9dded663981aa7c6ec45bdca5
crodociles <lukefroud86@gmail.com>
2017-12-20 14:44:58 +0000

    Initial commit

---
+++ Assets/Scripts/baddieControl.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class baddieControl : MonoBehaviour {
+
+    public Transform baddie;
+    public float moveSpeed = 3.00f;
+    public bool offScreen = true;
+    public float timer;
+    public System.Random r = new System.Random();
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update() {
+        transform.position += transform.right * -moveSpeed * Time.deltaTime;
+        if (baddie.GetComponent<Transform>().position.x < 6.3) {
+            moveSpeed = 0f;
+            timeToTurn();
+        }
+    }
+
+    void timeToTurn()
+    {
+        timer = r.Next(2, 10);
+        timer -= Time.deltaTime;
+        if (timer <= 0f)
+        {
+            this.GetComponent<SpriteRenderer>().flipX = false;
+        }
+    }
+}
